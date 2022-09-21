8934ab7dd0619812fc69c255968f0f1be83babdb
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-09-21 22:19:17 +0300

    CountingValley Challenge

---
+++ HackerRank/CountingValleys.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+
+   
+    let stack=[]
+    let sum=0
+    let valleys=0
+    
+    let patharr=[]
+    for(let i=0;i<path.length;i++)
+    {
+        patharr.push(path.charAt(i))
+    }
+    
+   
+
+    
+    for(let i=0;i<patharr.length;i++)
+    {
+        if(patharr.length<=0)
+        {
+            if(patharr[i]=="D")
+            {
+                stack.push("D")
+                sum+=1
+            }
+            else
+            {
+                stack.push("U")
+            }
+            
+        }
+        else(patharr.length>0)
+        {
+            if(patharr[i]=="D")
+            {
+                if(stack[stack.length-1]=="U")
+                {
+                    stack.pop()
+                }
+                else
+                {
+                    stack.push("D")
+                    sum+=1
+                }
+            }
+            else
+            {
+                if(stack[stack.length-1]=="D")
+                {
+                    stack.pop()
+                    let sumprev=sum
+                    sum-=1
+                    if(sumprev>0 && sum==0)
+                    {
+                        valleys+=1
+                    }
+                    
+                }
+                else
+                {
+                    stack.push("U")
+                }
+            }
+        }
+    }
+    
+    return valleys
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
