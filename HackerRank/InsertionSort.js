3afd557aa70f18a9eec80aeb5fd60b259101c9cf
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-09-21 12:17:26 +0300

    Insertion Sort Challenge

--- HackerRank/InsertionSort.js
+++ HackerRank/InsertionSort.js
@@ -1,5 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
 function insertionSort1(n, arr) {
-    // Write your code here
+       // Write your code here
 
     let indexoflastnum=arr.length-1
    
@@ -9,7 +39,6 @@ function insertionSort1(n, arr) {
        let printarr=[]
        if(arr[i-1]>arr[i])
        {
-      console.log(arr[i-1])
         let temp=arr[i-1]
         arr[i-1]=arr[i]
         arr[i]=temp
@@ -38,7 +67,10 @@ function insertionSort1(n, arr) {
     }
 }
 
+function main() {
+    const n = parseInt(readLine().trim(), 10);
 
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
 
-
-insertionSort1(5,[2,4,6,8,9,3])
+    insertionSort1(n, arr);
+}
