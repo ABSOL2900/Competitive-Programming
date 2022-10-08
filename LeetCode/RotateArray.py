6cfd0b9475ce51c4e3dcd52e013bff00c37e7a66
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-10-08 11:40:42 +0300

    Rotate Arrays

---
+++ LeetCode/RotateArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in range(k):
+            nums.insert(0,nums.pop())
+        
+        print(nums)
+            
+            
+            
+        
