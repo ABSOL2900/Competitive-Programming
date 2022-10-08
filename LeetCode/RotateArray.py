b99f274d5c8444f753aef6de5890cf58820859da
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-10-08 20:59:52 +0300

    Rotate Array

--- LeetCode/RotateArray.py
+++ LeetCode/RotateArray.py
@@ -3,11 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+
+        k=k%len(nums)
+        l=0
+        r=len(nums)-1
+        while l<r:
+            nums[l], nums[r]=nums[r], nums[l]
+            l+=1
+            r-=1
+            
+        l=0
+        r=k-1
+        while l<r:
+            nums[l], nums[r]=nums[r], nums[l]
+            l+=1
+            r-=1
+        l=k
+        r=len(nums)-1
+        while l<r:
+            nums[l], nums[r]=nums[r], nums[l]
+            l+=1
+            r-=1
+            
         
-        for i in range(k):
-            nums.insert(0,nums.pop())
-        
-        print(nums)
             
             
             
