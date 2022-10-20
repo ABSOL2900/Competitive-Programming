cfbb2fe36237da44e3d94474c8cf16f75107a5d6
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-10-20 23:16:21 +0300

    Validate Stack Sequence

---
+++ LeetCode/ValidateStackSequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        
+        j = 0
+ 
+        stack = []
+
+        for x in pushed:
+            stack.append(x)
+
+            while stack and j < len(popped) and stack[-1] == popped[j]:
+                stack.pop()
+                j = j + 1
+
+        return j == len(popped)
+                
