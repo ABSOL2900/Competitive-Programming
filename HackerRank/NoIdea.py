890f124233c8e6a656500815b293f11b3a59fe79
Abenazer Solomon <88853748+ABSOL2900@users.noreply.github.com>
2022-12-21 19:45:32 +0300

    No Idea

---
+++ HackerRank/NoIdea.py
@@ -0,0 +1,24 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def happiness():
+    
+    size=list(map(int,input().split(" ")))
+    arr=list(map(int,input().split(" ")))
+    a=set(map(int,input().split(" ")))
+    b=set(map(int,input().split(" ")))
+    happ=0
+    
+    for i in arr:
+        if i in a:
+            happ+=1
+        elif i in b:
+            happ-=1
+            
+    print(happ)
+    
+happiness()
+
+
+    
+    
+    
